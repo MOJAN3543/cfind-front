6dbc986ede5842806dcbbdea9d48173fc904e65f
MOJAN <sjs732@naver.com>
2024-11-22 13:42:08 +0900

    Fix: useSet 훅이 set을 설정 못하던 문제 해결

--- src/hooks/useSet.tsx
+++ src/hooks/useSet.tsx
@@ -6,12 +6,12 @@ export const useSet = <T,>(): UseSetReturnType<T> => {
     const [set, setSet] = useState<Set<T>>(new Set);
 
     const toggleSet = (element: T) => {
-        const updatedSet = new Set([...set]);
+        const updatedSet = set;
         if(updatedSet.has(element))
             updatedSet.delete(element);
         else
             updatedSet.add(element);
-        setSet(updatedSet);
+        setSet(new Set(updatedSet));
     }
 
     return [set, toggleSet];
